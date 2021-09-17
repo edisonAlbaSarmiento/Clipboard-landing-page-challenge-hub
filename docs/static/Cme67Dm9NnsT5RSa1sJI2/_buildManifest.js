a37495a14f2ad5dbf634548070fca43dd3efdb80
edison <edi9708@hotmail.com>
2021-09-16 20:26:49 -0500

    add folder deploy

---
+++ docs/static/Cme67Dm9NnsT5RSa1sJI2/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/3ee4778cba5051721c4a.css","static/chunks/pages/index-4c5257a0428f2cf374e6.js"],"/_error":["static/chunks/pages/_error-737a04e9a0da63c9d162.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
