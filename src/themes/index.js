e664cbc60df249d81bd12ab7f812b79e861c72a9
edison <edi9708@hotmail.com>
2021-11-29 22:55:50 -0500

    add new folders

---
+++ src/themes/index.js
@@ -0,0 +1,20 @@
+const colors = {
+	color: {
+		blueBackground: '#252a2e',
+		redtest: 'red',
+		trasnparentBackground: '#252b2e14',
+		titleBlue: '#0063FB',
+	},
+};
+
+const padding = {
+	paddingPrimary: {
+		paddingSmall: '3.3%',
+	},
+};
+const theme = {
+	colors,
+	padding,
+};
+
+export default theme;
